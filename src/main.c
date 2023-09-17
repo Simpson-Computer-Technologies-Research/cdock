1fee0c08501ce1cd9aa81acdbfd1fff996bbdcf2
Tristan Simpson <75189508+realTristan@users.noreply.github.com>
2023-09-17 13:24:22 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -11,22 +11,26 @@ typedef char *string;
 #define base_cmd_build "docker build -t "
 #define base_cmd_delete_image "docker rmi "
 #define base_cmd_delete_container "docker rm "
+#define base_cmd_exists_image "docker images -q "
 
 // Function prototypes
-void build_image(string);
-void build_container(string);
-void delete_image(string);
-void delete_container(string);
+int build_image(string);
+int build_container(string);
+int delete_image(string);
+int delete_container(string);
+int image_exists(string);
 
-void command_start(string);
-void command_new(string);
+int command_start(string);
+int command_new(string);
+int command_rmcontainer(string);
+int command_rmimage(string);
 
 int streq(string, string);
 
 // Main function
 int main(int argc, char **argv)
 {
-  if (argc != 3)
+  if (argc < 3)
   {
     printf("%s", error_message);
     return 1;
@@ -45,11 +49,11 @@ int main(int argc, char **argv)
   }
   else if (streq(command, "rmcontainer"))
   {
-    delete_container(name);
+    command_rmcontainer(name);
   }
   else if (streq(command, "rmimage"))
   {
-    delete_image(name);
+    command_rmimage(name);
   }
   else
   {
@@ -73,30 +77,89 @@ int streq(string s1, string s2)
 /**
  * Run the start command
  * @param name The container name
+ * @return 1 if successful, 0 if not
  */
-void command_start(string name)
+int command_start(string name)
 {
   printf("Building/Running container %s\n", name);
-  build_container(name);
+  return build_container(name);
 }
 
 /**
  * Run the new command
  * @param name The image name
+ * @return 1 if successful, 0 if not
  */
-void command_new(string name)
+int command_new(string name)
 {
+  // If the image already exists, delete it. If the deletion wasn't
+  // successful, exit the program.
+  printf("Checking if image %s already exists\n", name);
+  if (image_exists(name))
+  {
+    if (command_rmimage(name))
+    {
+      printf("Image %s already exists and was unable to be deleted.\n", name);
+      return 1;
+    }
+  }
+
+  // Build the image
   printf("Building image %s\n", name);
   build_image(name);
 
-  command_start(name);
+  // Run the container
+  return command_start(name);
+}
+
+/**
+ * Run the rmcontainer command
+ * @param name The container name
+ * @return 1 if successful, 0 if not
+ */
+int command_rmcontainer(string name)
+{
+  printf("Deleting container %s\n", name);
+  return delete_container(name);
+}
+
+/**
+ * Run the rmimage command
+ * @param name The image name
+ * @return 1 if successful, 0 if not
+ */
+int command_rmimage(string name)
+{
+  printf("Deleting image %s\n", name);
+  return delete_image(name);
+}
+
+/**
+ * Check if an image exists
+ * @param name The image name
+ * @return 1 if exists, 0 if not
+ */
+int image_exists(string name)
+{
+  const char *command = base_cmd_exists_image;
+  string command_with_name = malloc(strlen(command) + strlen(name) + 1);
+
+  // Size of: strlen(command)
+  strcpy(command_with_name, command);
+
+  // Size of: strlen(name)
+  strcat(command_with_name, name);
+
+  // Execute the command
+  return system(command_with_name) == 0;
 }
 
 /**
  * Build a new image
  * @param name The image name
+ * @return 1 if successful, 0 if not
  */
-void build_image(string name)
+int build_image(string name)
 {
   string build = base_cmd_build;
   string build_command = malloc(strlen(build) + strlen(name) + 2);
@@ -111,14 +174,15 @@ void build_image(string name)
   strcat(build_command, " .");
 
   // Execute the command
-  system(build_command);
+  return system(build_command);
 }
 
 /**
  * Build a new container
  * @param name The container name
+ * @return 1 if successful, 0 if not
  */
-void build_container(string name)
+int build_container(string name)
 {
   string run = base_cmd_run;
   string run_command = malloc(strlen(run) + strlen(name) * 2 + 2);
@@ -132,14 +196,15 @@ void build_container(string name)
   strcat(run_command, name);
 
   // Execute the command
-  system(run_command);
+  return system(run_command);
 }
 
 /**
  * Delete an image
  * @param name The image name
+ * @return 1 if successful, 0 if not
  */
-void delete_image(string name)
+int delete_image(string name)
 {
   string delete = base_cmd_delete_image;
   string delete_command = malloc(strlen(delete) + strlen(name) + 2);
@@ -151,14 +216,15 @@ void delete_image(string name)
   strcat(delete_command, name);
 
   // Execute the command
-  system(delete_command);
+  return system(delete_command) == 0;
 }
 
 /**
  * Delete a container
  * @param name The container name
+ * @return 1 if successful, 0 if not
  */
-void delete_container(string name)
+int delete_container(string name)
 {
   string delete = base_cmd_delete_container;
   string delete_command = malloc(strlen(delete) + strlen(name) + 2);
@@ -170,5 +236,5 @@ void delete_container(string name)
   strcat(delete_command, name);
 
   // Execute the command
-  system(delete_command);
+  return system(delete_command);
 }
